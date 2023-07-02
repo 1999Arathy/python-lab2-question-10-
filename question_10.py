dda07f7e772ee480e0a3716feb5eb660f9119731
1999Arathy <85410988+1999Arathy@users.noreply.github.com>
2023-07-02 20:53:27 +0530

    Add files via upload

---
+++ question_10.py
@@ -0,0 +1,5 @@
+#Write a Python program to reverse a tuple.
+
+tup = (4,8,"hai",9,456,25,78,91,100,10,"welcome")
+print("initial tuple is :", tup)
+print("reversed tuple is : ",tuple(reversed(tup)))
